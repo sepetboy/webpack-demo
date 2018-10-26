fc1a9b27a70883289a1b3459c88d9f8768392424
hutingjin <179645132@qq.com>
2018-10-26 16:11:14 +0800

    管理输出

---
+++ src/component-管理资源/index.js
@@ -0,0 +1,20 @@
+import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
+import Data from './data.xml';
+
+function component () {
+  var element = document.createElement('div');
+
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+  var image = new Image();
+  image.src = Icon;
+  element.appendChild(image);
+
+  console.log(Data);
+
+  return element;
+}
+
+document.body.appendChild(component());
--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
+import printMe from './print.js';
 
 function component () {
   var element = document.createElement('div');
+  var btn = document.createElement('button');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  var image = new Image();
-  image.src = Icon;
-  element.appendChild(image);
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
 
-  console.log(Data);
+  element.appendChild(btn);
 
   return element;
 }
